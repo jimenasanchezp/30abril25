43d1cfa93646be6dd25c78d667f1b7afee3ff41d
SANCHEZ PALOS JIMENA MONZERRAT <I24050019@monclova.tecnm.mx>
2025-05-14 20:28:42 -0600

    Agregar .gitattributes, .gitignore y README.md.

---
+++ 30abril25/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace _30abril25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrearFciheros = new System.Windows.Forms.Button();
+            this.btnFroma2Crear = new System.Windows.Forms.Button();
+            this.btnLeer1 = new System.Windows.Forms.Button();
+            this.btnLeer2 = new System.Windows.Forms.Button();
+            this.btnLeer3 = new System.Windows.Forms.Button();
+            this.btnLeer4 = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrearFciheros
+            // 
+            this.btnCrearFciheros.Location = new System.Drawing.Point(24, 12);
+            this.btnCrearFciheros.Name = "btnCrearFciheros";
+            this.btnCrearFciheros.Size = new System.Drawing.Size(87, 23);
+            this.btnCrearFciheros.TabIndex = 0;
+            this.btnCrearFciheros.Text = "Crear Fichero";
+            this.btnCrearFciheros.UseVisualStyleBackColor = true;
+            this.btnCrearFciheros.Click += new System.EventHandler(this.btnCrearFciheros_Click);
+            // 
+            // btnFroma2Crear
+            // 
+            this.btnFroma2Crear.Location = new System.Drawing.Point(117, 12);
+            this.btnFroma2Crear.Name = "btnFroma2Crear";
+            this.btnFroma2Crear.Size = new System.Drawing.Size(97, 23);
+            this.btnFroma2Crear.TabIndex = 1;
+            this.btnFroma2Crear.Text = "forma 2 de crear";
+            this.btnFroma2Crear.UseVisualStyleBackColor = true;
+            this.btnFroma2Crear.Click += new System.EventHandler(this.btnFroma2Crear_Click);
+            // 
+            // btnLeer1
+            // 
+            this.btnLeer1.Location = new System.Drawing.Point(220, 12);
+            this.btnLeer1.Name = "btnLeer1";
+            this.btnLeer1.Size = new System.Drawing.Size(97, 23);
+            this.btnLeer1.TabIndex = 2;
+            this.btnLeer1.Text = "Leer Archivo";
+            this.btnLeer1.UseVisualStyleBackColor = true;
+            this.btnLeer1.Click += new System.EventHandler(this.btnLeer1_Click);
+            // 
+            // btnLeer2
+            // 
+            this.btnLeer2.Location = new System.Drawing.Point(323, 12);
+            this.btnLeer2.Name = "btnLeer2";
+            this.btnLeer2.Size = new System.Drawing.Size(97, 23);
+            this.btnLeer2.TabIndex = 3;
+            this.btnLeer2.Text = "Leer Archivo";
+            this.btnLeer2.UseVisualStyleBackColor = true;
+            this.btnLeer2.Click += new System.EventHandler(this.btnLeer2_Click);
+            // 
+            // btnLeer3
+            // 
+            this.btnLeer3.Location = new System.Drawing.Point(24, 53);
+            this.btnLeer3.Name = "btnLeer3";
+            this.btnLeer3.Size = new System.Drawing.Size(97, 23);
+            this.btnLeer3.TabIndex = 4;
+            this.btnLeer3.Text = "Leer Archivo";
+            this.btnLeer3.UseVisualStyleBackColor = true;
+            this.btnLeer3.Click += new System.EventHandler(this.btnLeer3_Click);
+            // 
+            // btnLeer4
+            // 
+            this.btnLeer4.Location = new System.Drawing.Point(127, 53);
+            this.btnLeer4.Name = "btnLeer4";
+            this.btnLeer4.Size = new System.Drawing.Size(97, 23);
+            this.btnLeer4.TabIndex = 5;
+            this.btnLeer4.Text = "Leer Archivo";
+            this.btnLeer4.UseVisualStyleBackColor = true;
+            this.btnLeer4.Click += new System.EventHandler(this.btnLeer4_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(245, 53);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(97, 23);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(646, 307);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnLeer4);
+            this.Controls.Add(this.btnLeer3);
+            this.Controls.Add(this.btnLeer2);
+            this.Controls.Add(this.btnLeer1);
+            this.Controls.Add(this.btnFroma2Crear);
+            this.Controls.Add(this.btnCrearFciheros);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_CrearFichero;
+        private System.Windows.Forms.Button btn_CrearFichero2;
+        private System.Windows.Forms.Button btn_Leer;
+        private System.Windows.Forms.Button btn_DoLeer;
+        private System.Windows.Forms.Button btn_LeerFinal;
+        private System.Windows.Forms.Button btn_Abrir2;
+        private System.Windows.Forms.Button btn_Modificar;
+        private System.Windows.Forms.Button btnCrearFciheros;
+        private System.Windows.Forms.Button btnFroma2Crear;
+        private System.Windows.Forms.Button btnLeer1;
+        private System.Windows.Forms.Button btnLeer2;
+        private System.Windows.Forms.Button btnLeer3;
+        private System.Windows.Forms.Button btnLeer4;
+        private System.Windows.Forms.Button btnModificar;
+    }
+}
+
---
+++ 30abril25/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _30abril25
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCrearFciheros_Click(object sender, EventArgs e)
+        {
+            // StreamWrite es la clase que representa a un fichero de texto
+            //en el que podemos escribir informacion
+            //El fichero de texto lo creamos en el metodo (Estatico)CreateText,
+            //que pertenece a la clase File
+            //Paea escribir en el fichero de texto, utilizamos el metodo WriteLine
+            //Close para cerrar el archivo,o podria quedar algun dato
+            //sin guardar
+
+            StreamWriter fichero;
+            fichero = File.CreateText("fichero1.txt");
+            fichero.WriteLine("Hola este es mi primer ficher");
+            fichero.Write("Siguiente linea");
+            fichero.Write("\r\n");
+            fichero.WriteLine("Parte de la linea anterir");
+            fichero.Close();
+        }
+
+        private void btnFroma2Crear_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter fichero = new StreamWriter("fichero1.txt"))
+            {
+                fichero.WriteLine("Jimena");
+                fichero.WriteLine("Holis");
+                fichero.WriteLine("Hasta luego");
+            }
+        }
+
+        private void btnLeer1_Click(object sender, EventArgs e)
+        {
+            StreamReader fichero;
+            string linea;
+            fichero = File.OpenText("fichero1.txt");
+            linea = fichero.ReadLine();
+            Console.WriteLine(linea);
+
+            fichero.Close();
+        }
+
+        private void btnLeer3_Click(object sender, EventArgs e)
+        {
+            StreamReader fichero;
+            fichero = File.OpenText("fichero1.txt");
+            while (!fichero.EndOfStream)
+
+                Console.WriteLine(fichero.ReadLine());
+            fichero.Close();
+        }
+
+        private void btnLeer2_Click(object sender, EventArgs e)
+        {
+            StreamReader fichero;
+            string linea;
+            fichero = File.OpenText("fichero1.txt");
+            do
+            {
+                linea = fichero.ReadLine();
+                if (linea != null)
+                {
+                    Console.WriteLine(linea);
+                }
+            }
+            while (linea != null);
+            fichero.Close();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            StreamWriter fichero;
+
+            fichero = File.CreateText("fichero2.txt");
+            fichero.WriteLine("Primera linea");
+            fichero.Close();
+
+            fichero = File.AppendText("fichero2.txt");
+            fichero.WriteLine("Segunda linea");
+            fichero.Close();
+
+        }
+
+        private void btnLeer4_Click(object sender, EventArgs e)
+        {
+            using (StreamReader fichero = new StreamReader("fichero2.txt"))
+            {
+                string linea;
+                linea = fichero.ReadLine();
+                Console.WriteLine(linea);
+                Console.WriteLine(fichero.ReadLine());
+                Console.WriteLine(fichero.ReadLine());                
+            }
+        }
+    }    
+}
